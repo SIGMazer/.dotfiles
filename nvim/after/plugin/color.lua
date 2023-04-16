441c33c808c04788a5c07c2fffe0e1406dc751ad
SIGMazer <mazinasd7@gmail.com>
2023-04-16 23:22:27 +0200

    add remove bg

--- nvim/after/plugin/color.lua
+++ nvim/after/plugin/color.lua
@@ -1,2 +1,11 @@
 vim.cmd[[colorscheme dracula]]
+function ColorMyPencils(color) 
+	color = color or "dracula"
+	vim.cmd.colorscheme(color)
 
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+end
+
+ColorMyPencils()
--- nvim/after/plugin/nvim-tree.lua
+++ nvim/after/plugin/nvim-tree.lua
@@ -1,10 +1,16 @@
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+
 require("nvim-tree").setup({
   renderer = {
     icons = {
       show = {
         git = true,
 	file = false,
-	folder = false,
+	folder = true,
 	folder_arrow = true,
       },
       glyphs = {
