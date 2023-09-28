2f9173c7feffdd1f055390ee9f7eafe541610881
SIGMazer <mazinasd7@gmail.com>
2023-09-29 01:20:25 +0300

    color scheme

--- nvim/after/plugin/color.lua
+++ nvim/after/plugin/color.lua
@@ -1,6 +1,6 @@
-vim.cmd[[colorscheme catppuccin-macchiato]]
+vim.cmd[[colorscheme catppuccin-mocha]]
 function ColorZOpacity(color) 
-	color = color or "catppuccin-macchiato"
+	color = color or "catppuccin-mocha"
 	vim.cmd.colorscheme(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
